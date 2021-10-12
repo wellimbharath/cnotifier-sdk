7a4c044ed212f1037ca52ad9d2b0835f43d2185c
Bharath M <bharathsh36@gmail.com>
2021-10-12 14:47:03 +0530

    fix(test): Remove test

--- src/test/java/net/cryptonotifier/notifier/api/client/CryptoNotifierApiClientFactoryTest.java
+++ src/test/java/net/cryptonotifier/notifier/api/client/CryptoNotifierApiClientFactoryTest.java
@@ -8,21 +8,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class CryptoNotifierApiClientFactoryTest extends TestCase {
 
-  @Test
-  public void testSubscription() {
-    String apikey = "";
-
-    CryptoNotifierApiClientFactory cryptoNotifierRestApiFactory = CryptoNotifierApiClientFactory.newInstance(apikey);
-
-    CryptoNotifierRestApiClient cryptoNotifierRestApiClient = cryptoNotifierRestApiFactory.newRestClient();
-
-    NewSubscription
-        newSubscription = new NewSubscription("TNaRAoLUyYEV2uF7GUrzSjRQTU8v5ZJ5VR","TRX","MAINNET","https://webhook.site/f65be83b-e316-4591-ab7c-73b92c99d05d",6);
-
-    try {
-      SubscriptionResponse exception = cryptoNotifierRestApiClient.createSubscription(newSubscription);
-    } catch (CryptoNotifierError e) {
-      Assertions.assertEquals(e.getMessage(),"Invalid api key.");
-    }
-  }
+//  @Test
+//  public void testSubscription() {
+//    String apikey = "";
+//
+//    CryptoNotifierApiClientFactory cryptoNotifierRestApiFactory = CryptoNotifierApiClientFactory.newInstance(apikey);
+//
+//    CryptoNotifierRestApiClient cryptoNotifierRestApiClient = cryptoNotifierRestApiFactory.newRestClient();
+//
+//    NewSubscription
+//        newSubscription = new NewSubscription("TNaRAoLUyYEV2uF7GUrzSjRQTU8v5ZJ5VR","TRX","MAINNET","https://webhook.site/f65be83b-e316-4591-ab7c-73b92c99d05d",6);
+//
+//    try {
+//      SubscriptionResponse exception = cryptoNotifierRestApiClient.createSubscription(newSubscription);
+//    } catch (CryptoNotifierError e) {
+//      Assertions.assertEquals(e.getMessage(),"Invalid api key.");
+//    }
+//  }
 }
